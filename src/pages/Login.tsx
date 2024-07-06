c8b57e1f3703fb2ea50b551a8b1a8c45fce37f4b
Kamrul Hassan <kamrulthedev@gmail.com>
2024-07-06 13:36:27 +0600

    verify-token-commit

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,25 +1,41 @@
+import { Button } from "antd";
 import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useAppDispatch } from "../redux/features/hooks";
+import { setUser } from "../redux/features/auth/authSlice";
+import { verifyToken } from "../utils/verifyToken";
 
 const Login = () => {
-    const { register, handleSubmit } = useForm(); // Corrected the typo here
-  
-    const onSubmit = (data: any) => {
-      console.log(data);
+  const dispaths = useAppDispatch();
+  const { register, handleSubmit } = useForm({
+    defaultValues: { id: "A-0001", password: "Admin1234" },
+  });
+
+  const [Login, { error }] = useLoginMutation();
+
+  const onSubmit = async (data: any) => {
+    const userInfo = {
+      id: data.id,
+      password: data.password,
     };
-  
-    return (
-      <form style={{textAlign:'center' }} onSubmit={handleSubmit(onSubmit)}> {/* Corrected the typo here */}
-        <div>
-          <label htmlFor="id">ID: </label>
-          <input type="text" id="id" {...register('id')} />
-        </div>
-        <div>
-          <label htmlFor="password">Password: </label>
-          <input type="password" id="password" {...register('password')} />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    );
+    const res = await Login(userInfo).unwrap();
+    const user = verifyToken(res.data.accessToken);
+    dispaths(setUser({ user: user, token: res.data.accessToken }));
   };
-  
-  export default Login;
+
+  return (
+    <form style={{ textAlign: "center" }} onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID: </label>
+        <input type="text" id="id" {...register("id")} />
+      </div>
+      <div>
+        <label htmlFor="password">Password: </label>
+        <input type="password" id="password" {...register("password")} />
+      </div>
+      <Button htmlType="submit">Login</Button>
+    </form>
+  );
+};
+
+export default Login;
